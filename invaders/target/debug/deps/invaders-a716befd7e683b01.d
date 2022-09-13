a8f16d6eca88cfb046106f528fd718773a941ce7
JackofClubz <lancetersou@gmail.com>
2022-09-13 20:38:58 +0100

    Main

---
+++ invaders/target/debug/deps/invaders-a716befd7e683b01.d
@@ -0,0 +1,5 @@
+/Users/lance/invaders_rust/invaders/target/debug/deps/invaders-a716befd7e683b01.rmeta: src/main.rs
+
+/Users/lance/invaders_rust/invaders/target/debug/deps/invaders-a716befd7e683b01.d: src/main.rs
+
+src/main.rs:
